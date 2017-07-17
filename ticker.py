ec28df0969115079116ccb2f91043d3bffc419a6
damped <jonasmenge@shaw.ca>
2017-07-17 16:28:28 -0700

    added automatic trade data pulling

--- ticker.py
+++ ticker.py
@@ -3,11 +3,37 @@
 '''Used to continuesly pull market data'''
 
 import krakenex
+import json
 import pprint
+import time
 
 k = krakenex.API()
-k.load_key('kraken.key')
+#k.load_key('kraken.key')
 
 pp = pprint.PrettyPrinter(indent=4)
 
-pp.pprint(k.query_private('TradeBalance'))
+#pp.pprint(k.query_private('TradeBalance'))
+
+last = None
+
+while 1:
+
+	if last == None:
+		print("No last time")
+		try:
+			r = k.query_public('Trades', {'pair': 'XXBTZUSD'})
+		except:
+			print("Connecton Error...")
+			pass
+	else:
+		try:
+			r = k.query_public('Trades', {'pair': 'XXBTZUSD', 'since': 'last'})
+		except:
+			print("Connection Error...")
+			pass
+
+	last = r["result"]["last"]	
+
+	pp.pprint((last, r["result"]["XXBTZUSD"]))
+
+	time.sleep(6)
