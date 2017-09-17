871828d79940f2f7242d1cf8349bf0a7d64c0556
JonasM <jonasmenge@shaw.ca>
2017-09-17 15:47:59 -0700

    adding ticker file read funcionality (incompleat)

--- ticker.py
+++ ticker.py
@@ -19,41 +19,43 @@ last = None
 
 while 1:
 
-	if last == None:
-		print("No last time")
-		try:
-		    r = k.query_public('Trades', {'pair': 'XXBTZUSD'})
-		except:
-	    	    print("Connecton Error...")
-		    pass
-                try:
-                    
-                    with open('tradesLog.csv', 'r') as csvfile:
-                        reader = 
-                except:
-                    print("Error opening tradesLog.csv")
+    if last == None:
+        print("No last time")
+        try:
+            r = k.query_public('Trades', {'pair': 'XXBTZUSD'})
+        except:
+            print("Connecton Error...")
+            pass
+        
+        try:
+            with open('tradesLog.csv', 'r') as csvfile:
+                reader = csv.reader(csvfile, delimiter=' ', quoatechar='|')
+                for row in reader:
+                    print(row)
+        except:
+            print("Error opening tradesLog.csv")
 
-	else:
-		try:
-			r = k.query_public('Trades', {'pair': 'XXBTZUSD', 'since': last})
-		except:
-			print("Connection Error...")
-			pass
+    else:
+        try:
+            r = k.query_public('Trades', {'pair': 'XXBTZUSD', 'since': last})
+        except:
+            print("Connection Error...")
+            pass
 
-	last = r["result"]["last"]	
-	
-	data = r["result"]["XXBTZUSD"]	
-	
-	pp.pprint(data)
-	
-	with open('tradesLog.csv', 'a', newline='') as csvfile:
-		writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-	
-		for line in data:
-			writer.writerow(line)
+        last = r["result"]["last"]
 
-	
-	time.sleep(6)
+        data = r["result"]["XXBTZUSD"]
+
+        pp.pprint(data)
+
+        with open('tradesLog.csv', 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+            for line in data:
+                writer.writerow(line)
+
+
+        time.sleep(6)
 
 
 
