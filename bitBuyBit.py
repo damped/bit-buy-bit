a03ba9c455e91c92d83c0229b381f1aad394e4ae
JonasM <jonasmenge@shaw.ca>
2018-02-28 20:33:19 -0800

    simple avereging and automatic purging from memory

--- bitBuyBit.py
+++ bitBuyBit.py
@@ -6,18 +6,23 @@ import datetime
 import threading
 import queue
 
-#Data Logger
+#Data Logger and csv loading
 import csv
+import ast  # literal_eval() to convert strings to correct datatypes
 
 #API
 from bitex.api.WSS import BitstampWSS
 import logging
 import json
 
+import q # debuger
+
 
 # Defaults
 filename = "csv.log"
-fieldnames = ['amount', 'buy_order_id', 'sell_order_id', 'amount_str', 'price_str', 'timestamp', 'price', 'type', 'id']
+#fieldnames = ['amount', 'buy_order_id', 'sell_order_id', 'amount_str', 'price_str', 'timestamp', 'price', 'type', 'id']
+fieldnames = ['amount', 'buy_order_id', 'sell_order_id', 'timestamp', 'price', 'type', 'id']
+longAvgWindow = 60
 
 def main(filename):
     try:
@@ -57,7 +62,7 @@ def main(filename):
         analysisActive = threading.Event()
         analysisActive.set()
         t_analysis = threading.Thread(name='Analysis Thread',
-                target=analysis, args=(analysisActive, analysis_q, trading_q))
+                target=analysis, args=(analysisActive, analysis_q, trading_q, longAvgWindow))
 
         t_analysis.start()
 
@@ -75,17 +80,18 @@ def main(filename):
         t_dataLogger.join(0.1)
         t_analysis.join(0.1)
         
-        
 
-def resume(filename, outputQueues):
-    
-    try:
+
+
+
+def resume(filename, outputQueues): 
+    try:    # Try to open file
         with open(filename, 'r') as csvfile:
             print("Resumeing from file: " + filename)
-            reader = csv.DictReader(csvfile, fieldnames=fieldnames, delimiter=' ', quotechar='|')
+            reader = csv.DictReader(csvfile, delimiter=' ', quotechar='|')
             
-            all_lines = list(reader)
-            lastTime = int(all_lines[-1]["timestamp"])
+            all_lines = list(reader)                                # Each line is a dictonary
+            lastTime = int(all_lines[-1]["timestamp"])              # Get the -1th (aka last) line
             
             print('-' * 50)
             print("Last time in csv file: " + str(lastTime))
@@ -93,18 +99,34 @@ def resume(filename, outputQueues):
             print('-' * 50)
 
             for line in all_lines:
-                for q in outputQueues:
-                    q.put(line, block = True)
+                line = dict(line)                                   # Convert orderd dict to dict
+
+                line["amount"] = float(line["amount"])              # Convert strings to correct types
+                line["buy_order_id"] = int(line["buy_order_id"])
+                line["sell_order_id"] = int(line["sell_order_id"])
+                line["timestamp"] = int(line["timestamp"])
+                line["price"] = float(line["price"])
+                line["type"] = int(line["type"])
+                line["id"] = int(line["id"])
+
+
+                for qu in outputQueues:                             # Output to all queues supplied
+                    qu.put(line, block = True)                      # Block untill operation is compleat
     
-    except FileNotFoundError:
+    except FileNotFoundError:   # Make a new file with fieldname header
         print("File " + str(filename) + " does not exist. Creating...")
+
         with open(filename, 'w') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
             writer.writeheader()
 
     except:
-        #print("Error opening " + filename)
+        print("Error opening " + filename)
         raise
+
+
+
+
 
 def api(apiActive, outputQueues):
     log = logging.getLogger('bitstamp.api.WSS')
@@ -125,12 +147,22 @@ def api(apiActive, outputQueues):
                 data = json.loads(r[2])
                 #print(data["price"], data["timestamp"])
                 #dataLogger_q.put(data, block=True)
+
+                del data["amount_str"]  # Remove unnesasary string representations
+                del data["price_str"]
+
+                data["timestamp"] = int(data["timestamp"])  # Make timestamp and int
+
                 for q in outputQueues:
                     q.put(data, block=True)
 
             #wss.data_q.task_done()
             
     wss.stop()
+
+
+
+
 
 def dataLogger(dataLoggerActive, dataLogger_q, filename):
     while dataLoggerActive.isSet():
@@ -149,21 +181,57 @@ def dataLogger(dataLoggerActive, dataLogger_q, filename):
 
             dataLogger_q.task_done()
 
-def analysis(analysisActive, analysis_q, trading_q):
+
+
+
+
+def analysis(analysisActive, analysis_q, trading_q, longAvgWindow):
+
+    dataset = []    # Holds the currently relevent data for long and shortterm averaging
+    fresh = False   # Flag to indicate dat fresh data to process
+    oldestTime = 0  # Oldest time in dataset to trigger rescan
+
     while analysisActive.isSet():
+
+
         try:
             r = analysis_q.get(block=False)
         except queue.Empty:
+            # When we dont have any new data to process purge dataset and caclulat new average
+            if fresh or oldestTime <= int(time.time()) - longAvgWindow:
+                    fresh = False
+
+                    for line in dataset:
+                        if line["timestamp"] <= int(time.time()) - longAvgWindow:
+                            dataset.remove(line)
+
+                    if dataset: # if the list is not empty save the oldest timestamp
+                        oldestTime = dataset[0]["timestamp"]
+                        print("=" * 10)
+                        sumof = 0.0
+                        count = 0
+                        for line in dataset:
+                            print(line["price"])
+                            sumof += line["price"]
+                            count += 1
+                        print("Avg: " + str(sumof/count))
+                    
+
+
+            
             time.sleep(0.01)
             pass
         else:
-            print(r["price"])
 
-            #if r["timestamp"] >= 
+            if r["timestamp"] >= int(time.time()) - longAvgWindow:     # only add to averinging if its not too old
+                dataset.append(r)
+                fresh = True
 
 
 
             analysis_q.task_done()
+
+
 
 
 
@@ -173,6 +241,9 @@ def parseOptions():
             help=("File name to log to (default: " + filename + ")"))
 
     return parser.parse_args()
+
+
+
 
 if __name__ == '__main__':
 
